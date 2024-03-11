956f68b0f247269cf50fac44cb025e771c8abce8
OnurSevkiOkan <76997994+OnurSevkiOkan@users.noreply.github.com>
2024-03-11 08:39:49 +0300

    Update variables.h

--- game/src/variables.h
+++ game/src/variables.h
@@ -2,7 +2,17 @@
 #include <raylib.h>
 
 int targetFPS = 60;
+
 const int screen_width = 1000;
 const int screen_height = 1000;
+
 const float center_screenX = screen_width / 2;
 const float center_screenY = screen_height / 2;
+
+const float player_width = 128;
+const float player_height = 128;
+
+const float BULLET_SPEED = 150.0f; // constant speed for bullets
+
+Vector2 mouse_position = GetMousePosition();
+Vector2 playerPOS;
